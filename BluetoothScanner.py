266a7d60f51fd211a5136bf56b5e29e7384225f0
Shehbaz Khan <baz6066@gmail.com>
2016-10-29 21:40:08 -0500

    bluetooth scanner

--- BluetoothScanner.py
+++ BluetoothScanner.py
@@ -10,8 +10,9 @@ for addr, name in nearby_devices:
 
 import bluetooth
 
-nearby_devices = bluetooth.discover_devices(lookup_names=True)
-print("found %d devices" % len(nearby_devices))
+while True:
+    nearby_devices = bluetooth.discover_devices(lookup_names=True)
+    print("found %d devices" % len(nearby_devices))
 
 for addr, name in nearby_devices:
     print("  %s - %s" % (addr, name))
