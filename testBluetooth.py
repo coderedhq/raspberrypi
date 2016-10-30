5d3afdea51758efb20ea457e85231d18a6f9b511
Shehbaz Khan <baz6066@gmail.com>
2016-10-29 23:32:44 -0500

    bluetooth scanner

--- testBluetooth.py
+++ testBluetooth.py
@@ -1,5 +1,14 @@
-import subprocess
+import dbus
 
-proc = subprocess.Popen('ls', stdout=subprocess.PIPE)
-output = proc.stdout.read()
-print output
+bus = dbus.SystemBus()
+
+manager = dbus.Interface(bus.get_object('org.bluez', '/'), 'org.bluez.Manager')
+
+adapterPath = manager.DefaultAdapter()
+
+adapter = dbus.Interface(bus.get_object('org.bluez', adapterPath), 'org.bluez.Adapter')
+
+for devicePath in adapter.ListDevices():
+    device = dbus.Interface(bus.get_object('org.bluez', devicePath),'org.bluez.Device')
+    deviceProperties = device.GetProperties()
+    print deviceProperties["Address"]
