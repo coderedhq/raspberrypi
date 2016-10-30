166ada8b3a65b41f8662c2c3131d7bcdc8c66d8e
Ryan Morales <ryandmorales@yahoo.com>
2016-10-30 10:01:35 -0500

    initial commit

--- PushDevices.py
+++ PushDevices.py
@@ -1,16 +1,18 @@
 from firebase import firebase
+import sys
+import os
 
 firebase = firebase.FirebaseApplication('https://hacktathon.firebaseio.com', None)
 devices = {}
+ROOM_ID = 'abcdef'
 i = 0
+device_list = set()
 with open('sampleList') as f:
-    lines = f.readlines()
+    lines = f.readlines().splitlines()
     devices[i] = lines
+    identifiers = lines.split('|')
+    firebase.post('/rooms/{0}/devices/{1}/'.format(ROOM_ID, identifiers[0]), identifiers[1])
     i = i + 1
     max = i
-# push list to firebase
-for j in range(0, max):
-    print(devices[j])
-    j = j+1
 
-firebase.post('/devices', devices)
+
