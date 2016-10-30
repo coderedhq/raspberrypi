8631f56cdcbb83c2782aee6fd3c7616c7895d690
Shehbaz Khan <baz6066@gmail.com>
2016-10-29 23:11:59 -0500

    bluetooth scanner

--- testBluetooth.py
+++ testBluetooth.py
@@ -1,4 +1,8 @@
-import subprocess
+import subprocess, shlex
 
-p = subprocess.Popen("bluetoothctl");
-print (p)
+def subprocess_cmd(command):
+    process = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE)
+    proc_stdout = process.communicate()[0].strip()
+    print proc_stdout
+
+subprocess_cmd("sudo bluetoothctl; echo b")
